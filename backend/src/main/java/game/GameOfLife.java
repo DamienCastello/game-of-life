5d7f5eb3dbaf758fe7f0131d1afb8f7594d699cc
Damien Castello <d.castello.13200@gmail.com>
2026-07-01 13:44:49 +0200

    feat(next-generation): implement next generation

--- backend/src/main/java/game/GameOfLife.java
+++ backend/src/main/java/game/GameOfLife.java
@@ -30,13 +30,49 @@ public class GameOfLife {
 
                     if (newRow >= 0 && newRow < grid.length &&
                             newCol >= 0 && newCol < grid[0].length){
-                        neighbours ++;
-                        System.out.println(grid[newRow][newCol]);
+                        // Cellule voisine vivante
+                        if(grid[newRow][newCol]){
+                            neighbours ++;
+                        }
+                        //System.out.println(grid[newRow][newCol]);
                     }
                 }
             }
         }
         return neighbours;
+    }
+
+    public void nextGeneration(){
+        boolean[][] newGrid = new boolean[20][20];
+
+        for(int row = 0; row < grid.length; row ++){
+            for(int col = 0; col < grid[0].length; col ++){
+                int neighbours = countNeighbours(row, col);
+                // Cellule vivante
+                if(grid[row][col]){
+                    // Survie
+                    if(neighbours == 2 || neighbours == 3) {
+                        newGrid[row][col] = true;
+                    }
+                    // meurt par isolement ou surpopulation
+                    else {
+                        newGrid[row][col] = false;
+                    }
+                }
+                // Cellule morte
+                else {
+                    // Naissance
+                    if(neighbours == 3) {
+                        newGrid[row][col] = true;
+                    }
+                    // Pas de naissance
+                    else {
+                        newGrid[row][col] = false;
+                    }
+                }
+            }
+        }
+        grid = newGrid;
     }
 
     public boolean[][] getGrid() {
@@ -63,9 +99,18 @@ public class GameOfLife {
     public String toString() {
         StringBuilder sb = new StringBuilder();
 
-        for(boolean[] col : grid){
-            sb.append(Arrays.toString(col)).append("\n");
+        for (int row = 0; row < grid.length; row++) {
+            for (int col = 0; col < grid[0].length; col++) {
+
+                if (grid[row][col]) {
+                    sb.append("X ");
+                } else {
+                    sb.append(". ");
+                }
+            }
+            sb.append("\n");
         }
+
         return sb.toString();
     }
 }
--- backend/src/main/java/game/Start.java
+++ backend/src/main/java/game/Start.java
@@ -5,8 +5,36 @@ public class Start {
         boolean[][] grid = new boolean[20][20];
 
         GameOfLife game = new GameOfLife(grid);
-        game.getGrid()[0][0] = true;
+        game.getGrid()[0][1] = true;
+        game.getGrid()[1][2] = true;
+        game.getGrid()[2][0] = true;
+        game.getGrid()[2][1] = true;
+        game.getGrid()[2][2] = true;
         System.out.println(game);
-        game.countNeighbours(0, 1);
+        //game.countNeighbours(0, 1);
+        game.nextGeneration();
+        System.out.println("Next generation:");
+        System.out.println(game);
+        game.nextGeneration();
+        System.out.println("Next generation:");
+        System.out.println(game);
+        game.nextGeneration();
+        System.out.println("Next generation:");
+        System.out.println(game);
+        System.out.println("Next generation:");
+        System.out.println(game);
+        game.nextGeneration();
+        System.out.println("Next generation:");
+        System.out.println(game);
+        System.out.println("Next generation:");
+        System.out.println(game);
+        game.nextGeneration();
+        System.out.println("Next generation:");
+        System.out.println(game);
+        System.out.println("Next generation:");
+        System.out.println(game);
+        game.nextGeneration();
+        System.out.println("Next generation:");
+        System.out.println(game);
     }
 }
